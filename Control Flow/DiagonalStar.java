aa3f5c26792e7d1be7f780c3505c0e7604dddc40
VipulNerd <144052913+VipulNerd@users.noreply.github.com>
2024-07-12 00:06:42 +0530

    commit

--- Control Flow/DiagonalStar.java
+++ Control Flow/DiagonalStar.java
@@ -3,6 +3,7 @@ public class DiagonalStar {
     public static void printSquareStar(int number){
         if(number < 5){
             System.out.println("Invalid Value");
+            return;
         }
         for(int i =1; i <= number ; i++){
             for(int j = 1; j <= number; j++){
@@ -15,7 +16,7 @@ public class DiagonalStar {
         }
     }
     public static void main(String[] args) {
-        printSquareStar(5);
+        printSquareStar(2);
         printSquareStar(8);
     }
 }
