666f46d8c11b478ecde17262c279c5bcd6210ef5
VipulNerd <144052913+VipulNerd@users.noreply.github.com>
2024-07-30 18:04:48 +0530

    commit

--- Inheritance/Rectangle.java
+++ Inheritance/Rectangle.java
@@ -1,6 +1,6 @@
 public class Rectangle {
-    protected double width;
-    protected double length;
+    private double width;
+    private double length;
 
     public Rectangle(double width, double length){
         if (width < 0){
@@ -21,5 +21,44 @@ public class Rectangle {
     }
     public double getArea(){
         return width * length;
+    }
+}
+class Cuboid extends  Rectangle{
+
+    private double height;
+    
+    public Cuboid(double width, double length, double height){
+        super(width, length); 
+
+        if (height < 0){
+            height = 0;
+        }
+        
+        this.height = height;
+    }
+
+    public double getHeigth(){
+        return height;
+    }
+
+    public double getVolume(){
+        return this.getArea() * height;
+    }
+
+
+}
+
+class mainRC{
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeigth());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
     }
 }
