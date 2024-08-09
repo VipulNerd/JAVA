459dda202b96a6ee46ea90d3a9bca83d4f9fcbe4
VipulNerd <144052913+VipulNerd@users.noreply.github.com>
2024-08-09 23:03:50 +0530

    commit

---
+++ Array/practiseCase1.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Arrays;
+
+public class practiseCase1{
+    public static void main(String[] args) {
+        int[] firstArray = {20,23,44,56,23,85,58,99};
+        int[] secondArray = new int[firstArray.length];
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
+        for( int i = firstArray.length -1; i  >= 0; i--){
+            System.out.print(firstArray[i] + " ");
+        }
+        for (int i = 0; i < firstArray.length; i++){
+            secondArray[i] = firstArray[firstArray.length  - 1-i];
+            System.out.print(secondArray[i] + " ");
+        }
+        
+    }
+}
