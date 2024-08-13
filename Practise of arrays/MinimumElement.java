6a171ee03291fcb25984bef0b3536eca65d4c493
VipulNerd <144052913+VipulNerd@users.noreply.github.com>
2024-08-13 16:14:37 +0530

    commit

--- Practise of arrays/MinimumElement.java
+++ Practise of arrays/MinimumElement.java
@@ -1,36 +1,36 @@
 import java.util.Scanner;
 
-public class minimumElement {
-    public static int readInteger(){
+public class MinimumElement {
+    private static int readInteger(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number of elements to enter");
+        System.out.println("Enter count:");
         int size =  input.nextInt();
         return size;
     }
 
-    public static int[] readElements(int size){
+    private static int[] readElements(int size){
         Scanner element = new Scanner(System.in);
         int[] array = new int[size];
         System.out.println("Enter Elements of array:");
-        for( int i = 0 ; i < size; i++ ){
-           array[i] = element.nextInt();
+        for( int i = 0 ; i < array.length; i++ ){
+            System.out.println("Enter a number:");
+            array[i] = element.nextInt();
         }
-        System.out.println("Array of given size and element: " + array);
         return array;
 
     }
 
-    public static int findMin(int[] array){
+    private static int findMin(int[] array){
         int min = Integer.MAX_VALUE;
         for ( int i = 0;  i < array.length; i++ ){
-            int ele = array[i] ;
-            if (ele < min){
-                min = ele;
+            if (array[i] < min){
+                min = array[i];
             }
-            return min;
+            
         }
         return min;
     } 
+ 
     
     public static void main(String[] args) {
         int size = readInteger();
