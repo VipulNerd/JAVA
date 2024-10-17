abdd6fcd4f10c87b7ecd11b91825bc6b5cb8066d
VipulNerd <144052913+VipulNerd@users.noreply.github.com>
2024-10-17 22:11:00 +0530

    commit

--- Example of Abstraction/ListItem.java
+++ Example of Abstraction/ListItem.java
@@ -2,7 +2,7 @@
 public abstract class ListItem {
 
     protected ListItem rightLink = null;
-    protected ListItem lefttLink = null;
+    protected ListItem leftLink = null;
     protected Object value;
 
     public ListItem(Object value) {
@@ -18,13 +18,15 @@ public abstract class ListItem {
         this.value = value;
     }
 
-    abstract void next();
+    abstract ListItem next();
 
-    abstract void setNext();
+    abstract ListItem setNext(ListItem item);
 
-    abstract void previous();
+    abstract ListItem previous();
 
-    abstract void setPrevious();
+    abstract ListItem setPrevious(ListItem item);
+
+    abstract int compareTo(ListItem item);
 
 }
 
@@ -36,23 +38,116 @@ class Node extends ListItem {
     }
 
     @Override
-    void next() {
+    ListItem next() {
+        return this.rightLink;
+    }
+
+    @Override
+    public ListItem setNext(ListItem item) {
+        return this.rightLink = item;
+    }
+
+    @Override
+    ListItem previous() {
+        return this.leftLink;
+    }
+
+    @Override
+    ListItem setPrevious(ListItem item) {
+        return this.leftLink = item;
+    }
+
+    @Override
+    int compareTo(ListItem item) {
+        if (item != null) {
+            return ((String) this.value).compareTo((String) item.getValue());
+        } else {
+            return -1;
+        }
+    }
+
+}
+
+class MyLinkedList implements NodeList {
+
+    private ListItem root;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newitem) {
+        // initial entery
+        if (this.root != null) {
+            this.root = newitem;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int compareItem = currentItem.compareTo(newitem);
+            if (compareItem < 0) {
+                // if newItem is greater than currentItem
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    // at the end node
+                    currentItem.setNext(newitem);
+                    newitem.setPrevious(currentItem);
+                    return true;
+                }
+            } else if (compareItem > 0) {
+                // if newitem less than currentItem
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(newitem);
+                    newitem.setPrevious(currentItem.previous());
+                    newitem.setNext(currentItem);
+                    currentItem.setPrevious(newitem);
+                } else {
+                    // at start or node
+                    newitem.setNext(this.root);
+                    this.root.setPrevious(newitem);
+                    this.root = newitem;
+                }
+                return true;
+
+            } else {
+                // equal case
+                return false;
+            }
+
+        }
+        return false;
+
+    }
+
+    @Override
+    public boolean removItem() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    void setNext() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public ListItem traverse() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'traverse'");
     }
 
-    @Override
-    void previous() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+}
 
-    @Override
-    void setPrevious() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+interface NodeList {
+
+    ListItem getRoot();
+
+    boolean addItem(ListItem newitem);
+
+    boolean removItem();
+
+    ListItem traverse();
 
 }
