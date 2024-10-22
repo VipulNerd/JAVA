076e92714ec7e578cfe29abec0b1846e29645ad7
VipulNerd <144052913+VipulNerd@users.noreply.github.com>
2024-10-22 16:19:53 +0530

    commit

--- Example of interface/ player.java
+++ Example of interface/ player.java
@@ -1,16 +1,17 @@
 
+import java.util.ArrayList;
 import java.util.List;
 
-class player implements ISaveable {
+class Player implements ISaveable {
 
     private String name;
-    private String weapon;
     private int strength;
-    private int hitpoints;
+    private int hitPoints;
+    private String weapon;
 
-    public player(String name, int strength, int hitpoints) {
+    public Player(String name, int strength, int hitPoints) {
         this.name = name;
-        this.hitpoints = hitpoints;
+        this.hitPoints = hitPoints;
         this.strength = strength;
         this.weapon = "Sword";
     }
@@ -19,49 +20,126 @@ class player implements ISaveable {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getWeapon() {
         return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
     }
 
     public int getStrength() {
         return strength;
     }
 
-    public int getHitpoints() {
-        return hitpoints;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setWeapon(String weapon) {
-        this.weapon = "Sword";
-    }
-
     public void setStrength(int strength) {
         this.strength = strength;
     }
 
-    public void setHitPoints(int hitpoints) {
-        this.hitpoints = hitpoints;
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
     }
 
     @Override
     public List<String> write() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'write'");
+
+        List<String> values = new ArrayList<String>();
+        values.add(0, name);
+        values.add(1, "" + hitPoints);
+        values.add(2, "" + strength);
+        values.add(3, weapon);
+        return values;
     }
 
     @Override
-    public void read(List<String> values) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'read'");
+    public void read(List<String> savedValues) {
+
+        if (savedValues == null) {
+            return;
+        }
+        if (savedValues.size() <= 0) {
+            return;
+        }
+        name = savedValues.get(0);
+        hitPoints = Integer.parseInt(savedValues.get(1));
+        strength = Integer.parseInt(savedValues.get(2));
+        weapon = savedValues.get(3);
+    }
+
+    public String toString() {
+        return "Player{"
+                + "name='" + name + '\''
+                + ", hitPoints=" + hitPoints
+                + ", strength=" + strength
+                + ", weapon='" + weapon + '\''
+                + '}';
     }
 
 }
 
-class monter {
+class Monster implements ISaveable {
+
+    private String name;
+    private int hitPoints;
+    private int strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    @Override
+    public List<String> write() {
+
+        ArrayList<String> values = new ArrayList<String>();
+        values.add(0, name);
+        values.add(1, "" + hitPoints);
+        values.add(2, "" + strength);
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+
+        if (savedValues == null) {
+            return;
+        }
+        if (savedValues.size() <= 0) {
+            return;
+        }
+        name = savedValues.get(0);
+        hitPoints = Integer.parseInt(savedValues.get(1));
+        strength = Integer.parseInt(savedValues.get(2));
+    }
+
+    public String toString() {
+        return "Monster{"
+                + "name='" + name + '\''
+                + ", hitPoints=" + hitPoints
+                + ", strength=" + strength
+                + '}';
+    }
 
 }
 
@@ -69,5 +147,5 @@ interface ISaveable {
 
     List<String> write();
 
-    void read(List<String> values);
+    void read(List<String> savedValues);
 }
