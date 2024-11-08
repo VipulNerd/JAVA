60af2c4ae76dc4ddd559c95c9c24267c48365871
VipulNerd <144052913+VipulNerd@users.noreply.github.com>
2024-11-08 13:20:46 +0530

    commit

---
+++ Example of Lambda Expression/Challenge2.java
@@ -0,0 +1,30 @@
+
+import java.util.function.UnaryOperator;
+
+public class Challenge2 {
+
+    public static String everySecondChar(String source) {
+
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    }
+
+    public static void main(String[] args) {
+        UnaryOperator<String> everySecondChar = source -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+        System.out.println(everySecondChar.apply("12456789"));
+    }
+
+}
