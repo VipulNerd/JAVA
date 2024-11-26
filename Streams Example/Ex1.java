a31abc8157d5925b97fa72a16424cfeaae2a6bba
VipulNerd <144052913+VipulNerd@users.noreply.github.com>
2024-11-26 21:20:28 +0530

    commit

---
+++ Streams Example/Ex1.java
@@ -0,0 +1,19 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(5, 2, 9, 1, 6);
+        System.out.println(list.stream().map(x -> x * x).sorted().toList());
+
+        List<Integer> list2 = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(list2.stream().reduce(Integer::sum).get());
+
+        String word = "Hello World";
+        // char[] arr = word.toCharArray();
+
+        System.out.println(word.chars().filter(x -> x == 'l').count());
+    }
+}
